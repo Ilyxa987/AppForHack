e2359b78bb3921e0d298bc2afd216cb1559370e1
Ilyxa987 <eprigprogov228@gmail.com>
2023-04-01 18:00:25 +0300

    Fifth

--- app/src/main/java/com/example/appforhack/ui/tourTable/TourTable.java
+++ app/src/main/java/com/example/appforhack/ui/tourTable/TourTable.java
@@ -1,6 +1,31 @@
 package com.example.appforhack.ui.tourTable;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.appforhack.databinding.FragmentTableBinding;
+
 public class TourTable extends Fragment {
+
+    private FragmentTableBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentTableBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
 }
