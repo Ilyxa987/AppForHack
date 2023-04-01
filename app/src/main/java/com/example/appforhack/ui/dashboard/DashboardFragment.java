3553b1b80d5ebbfe9a73bcb28b5f9717cbc14600
Ilyxa987 <eprigprogov228@gmail.com>
2023-04-01 15:01:43 +0300

    Second

--- app/src/main/java/com/example/appforhack/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/appforhack/ui/dashboard/DashboardFragment.java
@@ -1,20 +1,27 @@
 package com.example.appforhack.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.appforhack.MainActivity;
+import com.example.appforhack.R;
 import com.example.appforhack.databinding.FragmentDashboardBinding;
+import com.example.appforhack.ui.games.HistoryOfCompany;
 
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends Fragment implements View.OnClickListener {
 
     private FragmentDashboardBinding binding;
+
+    ImageButton quest1, quest2, quest3;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +32,15 @@ public class DashboardFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
+
+        quest1 = root.findViewById(R.id.level1);
+        quest1.setOnClickListener(this);
+
+        quest2 = root.findViewById(R.id.level2);
+        quest2.setOnClickListener(this);
+
+        quest3 = root.findViewById(R.id.level3);
+        quest3.setOnClickListener(this);
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
@@ -33,5 +49,14 @@ public class DashboardFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.level1:
+                Intent intent = new Intent(getActivity(), HistoryOfCompany.class);
+                startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/example/appforhack/ui/games/HistoryOfCompany.java
@@ -0,0 +1,31 @@
+package com.example.appforhack.ui.games;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.appforhack.R;
+import com.example.appforhack.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HistoryOfCompany extends AppCompatActivity {
+
+    TextView quest1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.history_activity);
+
+
+
+    }
+
+
+}
